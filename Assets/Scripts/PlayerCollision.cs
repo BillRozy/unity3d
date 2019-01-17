d954cc42d36740fb51c0dd5d75feaba9d24b3fdd
Oleg Budylin <squee1994@gmail.com>
2019-01-17 20:55:53 +0300

    collision

---
+++ Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    public Rigidbody rb;
+    public PlayerMovement movement;
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+        if (collisionInfo.collider.tag == "Obstacle")
+        {
+            Debug.Log("Collision!");
+            rb.AddForce(0, 20000 * Time.deltaTime, 0);
+            movement.isEnabled = false;
+        }
+    }
+}
